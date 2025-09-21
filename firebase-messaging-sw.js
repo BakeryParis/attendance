1f9ae5db96b78cfefeb7ea0be2ceec51001d34df
BakeryParis <factorymis.bhs@gmail.com>
2025-09-21 18:54:43 +0530

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrSuvTsTd_o9quBiyq-GHkXKU3nYJPp7c",
+  authDomain: "parisbakerynotify.firebaseapp.com",
+  projectId: "parisbakerynotify",
+  storageBucket: "parisbakerynotify.firebasestorage.app",
+  messagingSenderId: "356112603370",
+  appId: "1:356112603370:web:36648e91bbe5b20f8b9f93"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+
+
+BMeR_RNuIsEMJ5Gdr3bpXVDdogfp4SRc7cPr70W1jlYVBzQXbrFd1H-J9_ptHA4PzJ7yWBKxbkOSjS81bHZ5DEs	
+
+snUdM6tMLu-o7pUk7p0DI2HffQtaDQsngTulmtOLW7E
